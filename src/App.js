58487d1757a1cf7858e829e6f00679b1db77f611
Karan Rathod <karanrathod@Karans-MacBook-Air-2.local>
2021-08-15 00:07:39 +0530

    cleanup extra code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-
 import AnecdoteForm from "./components/AnecdoteForm";
-import { increaseVote, newAnecdote } from "./reducers/anecdoteReducer";
 import AnecdoteList from "./components/AnecdoteList";
 
 const App = () => {
-  const anecdotes = useSelector((state) => state);
-  let sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes);
-  const dispatch = useDispatch();
-
-  const vote = (id) => {
-    dispatch(increaseVote(id));
-  };
-
   return (
     <div>
       <h2>Anecdotes</h2>
